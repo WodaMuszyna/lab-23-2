ea9dd041f68745b37b48e81a4bed09bda72e9bd2
WodaMuszyna <j878025@gmail.com>
2023-03-28 12:32:29 +0200

    bruh moment, fix my stupidness

--- lab5/zad4/wektory3d.c
+++ lab5/zad4/wektory3d.c
@@ -23,7 +23,7 @@ vec3d roznica(vec3d v1, vec3d v2){
 }
 
 float skalarny(vec3d v1, vec3d v2){
-    return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z + v2.z);
+    return ((v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z));
 }
 
 vec3d wektorowy(vec3d v1, vec3d v2){
